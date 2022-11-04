900160e36a4472b1862177e9bf4217037840daa2
ImnChert <nikitatishkov228@yandex.ru>
2022-11-04 10:28:27 +0300

    base response

--- ApplicationCore/Domain/Core/Interfaces/IBaseResponse.cs
+++ ApplicationCore/Domain/Core/Interfaces/IBaseResponse.cs
@@ -1,11 +1,10 @@
-﻿using ApplicationCore.Domain.Core.Enum;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ApplicationCore.Domain.Core.Interfaces
 {
 	public interface IBaseResponse<T>
 	{
 		T Data { get; set; }
-		public string Description { get; set; }
-		public StatusCode StatusCode { get; set; }
+		public StatusCodeResult StatusCode { get; set; }
 	}
 }
---
+++ ApplicationCore/Domain/Core/Models/BaseResponse.cs
@@ -0,0 +1,11 @@
+﻿using ApplicationCore.Domain.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationCore.Domain.Core.Models
+{
+	public class BaseResponse<T> : IBaseResponse<T>
+	{
+		public T Data { get; set; }
+		public StatusCodeResult StatusCode { get; set; }
+	}
+}
