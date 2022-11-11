334860400de0063a0700b0693ec1a50c2e831cca
ImnChert <nikitatishkov228@yandex.ru>
2022-11-11 17:09:20 +0300

    add equals and gethashcode

--- ApplicationCore/Domain/Core/Models/Cinema/Films/Distributor.cs
+++ ApplicationCore/Domain/Core/Models/Cinema/Films/Distributor.cs
@@ -3,5 +3,16 @@
 	public class Distributor : EntityBase
 	{
 		public string NameCompany { get; set; }
+
+		public override bool Equals(object? obj)
+		{
+			return obj is Distributor distributor &&
+				   NameCompany == distributor.NameCompany;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(NameCompany);
+		}
 	}
 }
