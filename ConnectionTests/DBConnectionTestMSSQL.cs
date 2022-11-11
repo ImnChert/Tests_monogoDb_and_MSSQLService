7c4076e1d4b76395a2941925dc5aa3caeff32acf
ImnChert <nikitatishkov228@yandex.ru>
2022-11-11 16:34:55 +0300

    successful testing of connection tests

---
+++ ConnectionTests/DBConnectionTestMSSQL.cs
@@ -0,0 +1,50 @@
+﻿using ApplicationCore.Services.Implementations.Repositories;
+using Infrastructure.Data.MSSQLServerRepository.Implementations.MajorRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System;
+
+namespace ConnectionTests
+{
+	public class DBConnectionTestMSSQL
+	{
+		[Fact]
+		public async Task TestMSServer_WithTheIncorrectPath_ThrowsException()
+		{
+			// Arrange
+			string connectionString = "path";
+			int planeId = -1;
+			var planeRepository = new CategoryRepository(connectionString);
+			var planeService = new CategoryService(planeRepository);
+
+			// Act
+			var result = await planeService.GetById(planeId);
+
+			// Assert
+			Assert.Equal((new BadRequestResult()).StatusCode, result.StatusCode.StatusCode);
+		}
+
+		[Fact]
+		public async Task TestMSServer_WithTheCorrectPath_True()
+		{
+			// Arrange
+			string connectionString = @"Data Source=DESKTOP-CTBUCT0\SQLEXPRESS;;Initial Catalog=CP;Integrated Security=True";
+			int planeId = -1;
+			var planeRepository = new CategoryRepository(connectionString);
+			var planeService = new CategoryService(planeRepository);
+
+			// Act
+			DateTime startTime = DateTime.Now;
+			await planeService.GetById(planeId);
+
+			if (DateTime.Now.Subtract(startTime) >= new TimeSpan(0, 0, 0, 0, 10000))
+			{
+				Assert.True(false);
+			}
+
+			// Assert
+			Assert.True(true);
+		}
+
+	}
+}
---
+++ ConnectionTests/DBConnectionTestMongo.cs
@@ -0,0 +1,53 @@
+﻿using ApplicationCore.Services.Implementations.Repositories;
+using Infrastructure.Data.MongoRepository.Implementations;
+
+namespace ConnectionTests
+{
+	public class DBConnectionTestMongo
+	{
+		[Fact]
+		public async Task TestMongoDB_WithTheIncorrectPath_ThrowsException()
+		{
+			string connectionString = "path";
+			int planeId = -1;
+
+			try
+			{
+				var planeRepository = new CategoryRepository(connectionString);
+				var planeService = new CategoryService(planeRepository);
+
+				var result = await planeService.GetById(planeId);
+			}
+			catch (Exception ex)
+			{
+				Assert.True(true);
+				return;
+			}
+
+			Assert.True(false);
+		}
+
+		[Fact]
+		public async Task TestMongoDB_WithTheCorrectPath_True()
+		{
+			// Arrange
+			string connectionString = "mongodb://localhost:27017";
+			int planeId = -1;
+
+			var planeRepository = new CategoryRepository(connectionString);
+			var planeService = new CategoryService(planeRepository);
+
+			// Act
+			DateTime startTime = DateTime.Now;
+			var result = await planeService.GetById(planeId);
+
+			if (DateTime.Now.Subtract(startTime) >= new TimeSpan(0, 0, 0, 0, 10000))
+			{
+				Assert.True(false);
+			}
+
+			// Assert
+			Assert.True(true);
+		}
+	}
+}
