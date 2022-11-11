02c28a9eea72e738b236419d1f6d19130b72cad7
ImnChert <nikitatishkov228@yandex.ru>
2022-11-11 12:09:05 +0300

    co

---
+++ TestCons/OutputData.cs
@@ -0,0 +1,11 @@
+﻿namespace TestConsole
+{
+	internal class OutputData<T>
+	{
+		public void OutputList(List<T> list)
+			=> list.ForEach(x => Console.WriteLine(x));
+
+		public void OutputObject(T obj)
+			=> Console.WriteLine(obj);
+	}
+}
