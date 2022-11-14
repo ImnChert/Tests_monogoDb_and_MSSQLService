7d83192dd4770bd148d6c30a9f1060ed13d7709e
ImnChert <nikitatishkov228@yandex.ru>
2022-11-14 12:13:17 +0300

    realisation review

--- Infrastructure/Data/MSSQLServerRepository/Implementations/MajorRepository/ReviewRepository.cs
+++ Infrastructure/Data/MSSQLServerRepository/Implementations/MajorRepository/ReviewRepository.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ApplicationCore.Domain.Core.Models.Cinema;
+using ApplicationCore.Domain.Core.Models.Cinema.Films;
+using ApplicationCore.Domain.Core.Models.Roles;
+using ApplicationCore.Domain.Interfaces.Interfaces;
+using Infrastructure.Data.MSSQLServerRepository.Connection;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Infrastructure.Data.MSSQLServerRepository.Implementations.MajorRepository
 {
-	internal class ReviewRepository
+	internal class ReviewRepository : MainMSSQLServerRepository<Review>
 	{
+		private readonly Film _film;
+		private readonly IRepository<RegisteredUser> _registerUserRepository;
+
+		public ReviewRepository(string connectionString, IRepository<RegisteredUser> registerUserRepository, Film film)
+			: base(connectionString,
+				  "Reviews",
+				  $@"INSERT INTO Reviews (Id,FilmId,RegisteredUsersId,DescriptionReview) 
+					VALUES(@Id,@FilmId,@RegisteredUsersId,@DescriptionReview)",
+				  $@"UPDATE Reviews SET FilmId=@FilmId, RegisteredUsersId=@RegisteredUsersId, DescriptionReview=@DescriptionReview 
+					WHERE Id=@Id",
+				  $"SELECT Id,FilmId,RegisteredUsersId,DescriptionReview FROM Reviews",
+				  $"SELECT Id,FilmId,RegisteredUsersId,DescriptionReview FROM Reviews WHERE Id=@id",
+				  $"SELECT Id,FilmId,RegisteredUsersId,DescriptionReview FROM Reviews WHERE FilmsId=@FilmsId",
+				  "@FilmsId")
+		{
+			_registerUserRepository = registerUserRepository;
+			_film = film;
+		}
+
+		protected override Review GetReader(SqlDataReader sqlDataReader)
+			=> new Review()
+			{
+				Id = (int)sqlDataReader["Id"],
+				RegisteredUser = _registerUserRepository.GetById((int)sqlDataReader["RegisteredUsersId"]).Re,
+				Description = sqlDataReader["Description"] as string ?? "Undefined"
+			};
+
+		protected override void InsertCommand(SqlCommand sqlCommand, Review entity)
+		{
+			sqlCommand.Parameters.Add("@RegisteredUsersId", SqlDbType.NVarChar).Value = entity.RegisteredUser.Id;
+			sqlCommand.Parameters.Add("@FilmId", SqlDbType.NVarChar).Value = _film.Id;
+			sqlCommand.Parameters.Add("@Description", SqlDbType.Decimal).Value = entity.Description;
+		}
 	}
 }
