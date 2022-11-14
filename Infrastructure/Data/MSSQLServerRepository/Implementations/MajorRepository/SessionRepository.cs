2a03e32f660c580840059870940c4f370b9d609e
ImnChert <nikitatishkov228@yandex.ru>
2022-11-14 10:36:05 +0300

    add session repository

--- Infrastructure/Data/MSSQLServerRepository/Implementations/MajorRepository/SessionRepository.cs
+++ Infrastructure/Data/MSSQLServerRepository/Implementations/MajorRepository/SessionRepository.cs
@@ -1,6 +1,9 @@
 ﻿using ApplicationCore.Domain.Core.Models.Cinema;
+using ApplicationCore.Domain.Core.Models.Cinema.Films;
 using ApplicationCore.Domain.Interfaces.Interfaces;
+using Infrastructure.Data.MongoRepository.Implementations;
 using Infrastructure.Data.MSSQLServerRepository.Connection;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace Infrastructure.Data.MSSQLServerRepository.Implementations.MajorRepository
@@ -8,26 +11,32 @@ namespace Infrastructure.Data.MSSQLServerRepository.Implementations.MajorReposit
 	internal class SessionRepository : MainMSSQLServerRepository<Session>
 	{
 		private IRepository<Ticket> _ticketRepository;
+		private IRepository<Film> _filmRepository;
 
-		public SessionRepository(string connectionString, IRepository<Ticket> ticketRepository)
+		public SessionRepository(string connectionString, IRepository<Ticket> ticketRepository, IRepository<Film> filmRepository)
 			: base(connectionString,
 				  "SessionsOfFilms",
-				  insertQuery,
-				  updateQuery,
-				  getAllQuery,
-				  getByIdQuery)
+				  $"INSERT INTO SessionsOfFilms (Id,FilmId,StartTime) VALUES(@Id,@FilmId,@StartTime)",
+				  $"UPDATE SessionsOfFilms SET FilmId=@FilmId, StartTime=@StartTime WHERE Id=@Id",
+				  $"SELECT Id, FilmId, StartTime FROM SessionsOfFilms",
+				  $"SELECT Id, FilmId, StartTime FROM SessionsOfFilms WHERE Id=@id")
 		{
 			_ticketRepository = ticketRepository;
+			_filmRepository = filmRepository;
 		}
 
 		protected override Session GetReader(SqlDataReader sqlDataReader)
-		{
-			throw new NotImplementedException();
-		}
+		  => new Session()
+		  {
+			  Id = (int)sqlDataReader["Id"],
+			  Film = _filmRepository.GetById((int)sqlDataReader["FilmId"]).Result,
+			  StartTime = DateTime.Parse(sqlDataReader["StartTime"].ToString())
+		  };
 
 		protected override void InsertCommand(SqlCommand sqlCommand, Session entity)
 		{
-			throw new NotImplementedException();
+			sqlCommand.Parameters.Add("@FilmId", SqlDbType.NVarChar).Value = entity.Film.Id;
+			sqlCommand.Parameters.Add("@StartTime", SqlDbType.Decimal).Value = entity.StartTime;
 		}
 	}
 }
