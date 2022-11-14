70ffca95a0814b50e630b718ca7d3abd68a2f510
ImnChert <nikitatishkov228@yandex.ru>
2022-11-14 11:36:55 +0300

    realisation distributrepository

--- Infrastructure/Data/MSSQLServerRepository/Implementations/MajorRepository/DistributorRepository.cs
+++ Infrastructure/Data/MSSQLServerRepository/Implementations/MajorRepository/DistributorRepository.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ApplicationCore.Domain.Core.Models.Cinema.Films;
+using Infrastructure.Data.MSSQLServerRepository.Connection;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Infrastructure.Data.MSSQLServerRepository.Implementations.MajorRepository
 {
-	internal class DistributorRepository
+	internal class DistributorRepository : MainMSSQLServerRepository<Distributor>
 	{
+		public DistributorRepository(string connectionString)
+			: base(connectionString,
+				  "Distributors",
+				  $"INSERT INTO Distributors (Id,NameCompany) VALUES(@Id,@NameCompany)",
+				  $"UPDATE Distributors SET NameCompany=@NameCompany WHERE Id=@Id",
+				  $"SELECT Id,NameCompany FROM Distributors",
+				  $"SELECT Id,NameCompany FROM Distributors WHERE Id=@id")
+		{
+		}
+
+		protected override Distributor GetReader(SqlDataReader sqlDataReader)
+		=> new Distributor()
+		{
+			Id = (int)sqlDataReader["Id"],
+			NameCompany = sqlDataReader["Name"] as string ?? "Undefined",
+		};
+
+		protected override void InsertCommand(SqlCommand sqlCommand, Distributor entity)
+		{
+			sqlCommand.Parameters.Add("@NameCompany", SqlDbType.NVarChar).Value = entity.NameCompany;
+		}
 	}
 }
