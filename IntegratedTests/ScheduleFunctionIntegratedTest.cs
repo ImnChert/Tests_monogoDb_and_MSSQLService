c1d2a1780aaad9f5a19f18ce163751e5a1230257
ImnChert <nikitatishkov228@yandex.ru>
2022-11-18 12:31:18 +0300

    integrative test

--- IntegratedTests/ScheduleFunctionIntegratedTest.cs
+++ IntegratedTests/ScheduleFunctionIntegratedTest.cs
@@ -1,22 +1,162 @@
 ﻿using ApplicationCore.Domain.Core.Models.Cinema;
-using ApplicationCore.Services.Implementations.Functional_entities;
-using ApplicationCore.Services.Implementations.Validations;
-using ApplicationCore.Services.Interfaces.Functional_entities;
+using ApplicationCore.Domain.Core.Models.Cinema.Films;
+using ApplicationCore.Domain.Core.Models.Roles;
+using ApplicationCore.Domain.Core.Models.Roles.Staff;
+using ApplicationCore.Domain.Core.Models.Roles.Staff.Positions;
+using ApplicationCore.Services.Implementations.Repositories;
+using Infrastructure.Data.MongoRepository.Implementations;
 
 namespace IntegratedTests
 {
 	[TestCaseOrderer("IntegratedTests.Configuration", "IntegratedTests")]
 	public class ScheduleFunctionIntegratedTest
 	{
-		private readonly string _connectionString
-			= @"Data Source=DESKTOP-CTBUCT0\SQLEXPRESS;;Initial Catalog=CP;Integrated Security=True";
+		private string _connectionString = "mongodb://localhost:27017";
+		//@"Data Source=DESKTOP-CTBUCT0\SQLEXPRESS;Initial Catalog=CP;Integrated Security=True";
+
+		private List<RegisteredUser> GetTestUsers()
+			=> new List<RegisteredUser>()
+			{
+				new RegisteredUser()
+				{
+					Username = "test3",
+					Password = "test3",
+					FirstName = "test3",
+					MiddleName = "test3",
+					LastName = "test3",
+					DateOfBirthday = DateTime.Now,
+					Phone = "+375444444444"
+				},
+				new RegisteredUser()
+				{
+					Username = "test3",
+					Password = "test3",
+					FirstName = "test3",
+					MiddleName = "test3",
+					LastName = "test3",
+					DateOfBirthday = DateTime.Now,
+					Phone = "+375444444444"
+				},
+				new RegisteredUser()
+				{
+					Username = "test3",
+					Password = "test3",
+					FirstName = "test3",
+					MiddleName = "test3",
+					LastName = "test3",
+					DateOfBirthday = DateTime.Now,
+					Phone = "+375444444444"
+				}
+			};
+
+		private Category GetTestCategory()
+			=> new Category()
+			{
+				Name = "test",
+				Price = 100
+			};
+
+		private Film GetTestFilm()
+			=> new Film()
+			{
+				Name = "test",
+				Duration = 120,
+				FilmCrew = new List<Person>()
+						{
+							new Person()
+							{
+								FirstName= "testFirstName",
+								MiddleName = "testMiddleName",
+								LastName = "testLastName",
+								Post = "Режисер"
+							}
+						},
+				Reviews = new List<Review>()
+						{
+							new Review()
+							{
+								RegisteredUser = GetTestUsers()[0],
+								Description = "test"
+							}
+						},
+				Scores = new List<Score>()
+						{
+							new Score()
+							{
+								RegisteredUser = GetTestUsers()[0],
+								Raiting = 1
+							}
+						},
+				Description = "test",
+				Year = 2001,
+				LicensExpirationDate = DateTime.Now,
+				Distributor = new Distributor()
+				{
+					NameCompany = "test"
+				},
+				BasePrice = 100
+			};
+
+		private Employee GetTestEmployee()
+			=> new Employee()
+			{
+				Username = "test3",
+				Password = "test3",
+				FirstName = "test3",
+				MiddleName = "test3",
+				LastName = "test3",
+				Positions = new List<Position>()
+				{
+					new Cashier()
+				}
+			};
+
+		private List<Ticket> GetTestTicket()
+			=> new List<Ticket>()
+					{
+						new Ticket()
+						{
+							Seat = new Seat()
+							{
+								NumberRow = 1,
+								NumberColumn = 1,
+								Category =  null
+							},
+							RegisteredUser = null,
+							Cashier = null
+						},
+						new Ticket()
+						{
+							Seat = new Seat()
+							{
+								NumberRow = 2,
+								NumberColumn = 1,
+								Category = null
+							},
+							RegisteredUser = null,
+							Cashier = null
+						}
+					}
+
+
+		private List<Session> GetTestListSession()
+			=> new List<Session>()
+			{
+				new Session()
+				{
+					Film = null,
+					StartTime= DateTime.Now,
+					Tickets =  null
+				}
+			};
 
 		private Schedule GetTestSchedule()
 			=> new Schedule
 			{
 				Id = 1,
 				Hall = new Hall { Id = 1 },
-				Date = new DateTime()
+				Date = new DateTime(),
+				Sessions = GetTestListSession()
 			};
 
 		[Fact, TestPriority(0)]
@@ -25,58 +165,97 @@ namespace IntegratedTests
 			try
 			{
 				// Arrange
+				var userRepository = new UserRepository(_connectionString);
+				var userService = new UserService(userRepository);
+
+				var employeeRepository = new EmployeeRepository(_connectionString);
+				var employeeService = new EmployeeService(employeeRepository);
+
+				var filmRepositpry = new FilmRepository(_connectionString);
+				var filmService = new FilmService(filmRepositpry);
+
+
+
+				var scheduleRepository = new ScheduleRepository(_connectionString, filmRepositpry);
+				var scheduleService = new ScheduleService(scheduleRepository);
+
+				List<RegisteredUser> collectionUsers = GetTestUsers();
+				Category category = GetTestCategory();
+				Employee employee = GetTestEmployee();
 				Schedule schedule = GetTestSchedule();
-				var scheduleFunction = new ScheduleFunction(new ScheduleValidation(schedule), schedule);
-
-				IPlaneService planeService = new PlaneService(new PlaneRepositoryMS(_stringConnection));
-				ICountryService countryRepository = new CountryService(new CountryRepositoryMS(_stringConnection));
-				ICityService cityService = new CityService(new CityRepositoryMS(_stringConnection));
-				ITripService tripService = new TripService(new TripRepositoryMS(_stringConnection));
-				ITripDetailService tripDetailService = new TripDetailService(new TripDetailRepositoryMS(_stringConnection),
-					new TripRepositoryMS(_stringConnection), new UserRepositoryMS(_stringConnection));
-
-				var plane = GetPlane();
-				var countryName = "countryName";
-				string firstCityName = "some1";
-				string secondCityName = "some2";
-
-				Trip addTrip = new Trip()
-				{
-					StartDate = new DateTime(2022, 11, 20, 9, 00, 00),
-					EndDate = new DateTime(2022, 11, 20, 11, 30, 00),
-					Capacity = 10,
-					Price = 40
-				};
 
 				// Act
-				await planeService.Create(plane);
-				var planeData = await planeService.GetByName(plane.Name);
-				await countryRepository.Create(new Country() { Name = countryName });
-				var countryData = await countryRepository.GetAll();
-				int countryId = countryData.Data.Max(x => x.Id);
 
-				await cityService.Create(new City() { Name = firstCityName, CountryId = countryId });
-				await cityService.Create(new City() { Name = secondCityName, CountryId = countryId });
+				employeeRepository.InsertAsync(employee);
+				Employee employeeData = employeeRepository.GetAllAsync().Result[0];
 
-				var foundcity1 = await cityService.GetByCityName(firstCityName);
-				var foundcity2 = await cityService.GetByCityName(secondCityName);
-				addTrip.StartCityId = foundcity1.Data.Id;
-				addTrip.EndCityId = foundcity2.Data.Id;
-				addTrip.PlaneId = planeData.Data.Id;
+				collectionUsers.ForEach(async user => await userService.InsertAsync(user));
+				List<RegisteredUser> usersData = (await userService.GetAllAsync()).Result;
 
-				await tripService.Create(addTrip);
-				var trips = await tripService.GetAll();
-
-				_foundTrip = trips.Data.Last();
-				await tripDetailService.Create(new TripDetails() { Place = 1, UserId = 3, TripId = _foundTrip.Id });
-				await tripDetailService.Create(new TripDetails() { Place = 2, UserId = 3, TripId = _foundTrip.Id });
-				await tripDetailService.Create(new TripDetails() { Place = 3, UserId = 3, TripId = _foundTrip.Id });
-				await tripDetailService.Create(new TripDetails() { Place = 4, UserId = 3, TripId = _foundTrip.Id });
-				await tripDetailService.Create(new TripDetails() { Place = 6, UserId = 3, TripId = _foundTrip.Id });
+				await scheduleService.InsertAsync(schedule);
+				var scheduleData = await scheduleService.GetById(schedule.Id);
 
 				Assert.True(true);
 			}
-			catch (System.Exception)
+			catch (Exception)
+			{
+
+				Assert.True(false);
+			}
+		}
+
+		//[Fact, TestPriority(1)]
+		//public async Task TestCalculation()
+		//{
+		//	// Arrange
+		//	ITripDetailService tripDetailService = new TripDetailService(new TripDetailRepositoryMS(_stringConnection),
+		//		new TripRepositoryMS(_stringConnection), new UserRepositoryMS(_stringConnection));
+		//	ITripTreatmentService tripTreatmentService = new TripTreatmentService();
+
+		//	// Act
+		//	var data = await tripDetailService.GetDetailsByTripId(_foundTrip.Id);
+
+		//	var availablePlacesResponse = tripTreatmentService.GetAvailablePlaces(data.Data, _foundTrip.Capacity);
+		//	var aavailablePlaces = availablePlacesResponse.Data;
+		//	// Assert
+
+		//	Assert.Equal(_foundTrip.Capacity - data.Data.Count(), aavailablePlaces.Count());
+		//}
+
+		[Fact, TestPriority(1)]
+		public async Task TestDeleteAllData()
+		{
+			try
+			{
+				// Arrange
+				var userRepository = new UserRepository(_connectionString);
+				var userService = new UserService(userRepository);
+
+				//var ticketRepositpry = new TicketRepository(_connectionString);
+				var filmRepositpry = new FilmRepository(_connectionString);
+
+
+				var scheduleRepository = new ScheduleRepository(_connectionString, filmRepositpry);
+				var scheduleService = new ScheduleService(scheduleRepository);
+
+				// Act
+				//var details = await tripDetailService.GetDetailsByTripId(_foundTrip.Id);
+				//await DeleteTripDetails(details.Data);
+				//await tripService.DeleteById(_foundTrip.Id);
+				//await planeService.DeleteById(_foundTrip.PlaneId);
+
+				//var country = await cityService.GetById(_foundTrip.StartCityId);
+
+				//await cityService.DeleteById(_foundTrip.StartCityId);
+				//await cityService.DeleteById(_foundTrip.EndCityId);
+				//await countryService.DeleteById(country.Data.CountryId);
+				var s = userService.GetById(1).Result;
+				//await userService.DeleteAsync(GetTestUser());
+				//await scheduleService.DeleteAsync(GetTestSchedule());
+
+				Assert.True(true);
+			}
+			catch (Exception)
 			{
 
 				Assert.True(false);
