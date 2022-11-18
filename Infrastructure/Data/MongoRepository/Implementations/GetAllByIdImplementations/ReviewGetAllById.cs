0ac2ed9f59067efd9e5793d31b23e4075dedd6e4
ImnChert <nikitatishkov228@yandex.ru>
2022-11-18 16:38:30 +0300

    rewiev realisation

---
+++ Infrastructure/Data/MongoRepository/Implementations/GetAllByIdImplementations/ReviewGetAllById.cs
@@ -0,0 +1,66 @@
+﻿using ApplicationCore.Domain.Core.Models.Cinema.Films;
+using ApplicationCore.Domain.Core.Models.Roles;
+using ApplicationCore.Domain.Interfaces;
+using ApplicationCore.Domain.Interfaces.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Infrastructure.Data.MongoRepository.Implementations.GetAllByIdImplementations
+{
+	internal class ReviewGetAllById : IGetAllById<Review>
+	{
+		private readonly IMongoCollection<BsonDocument> _mongoCollection;
+		private IRepository<RegisteredUser> _registeredUserRepository;
+
+		public ReviewGetAllById(string connectionString, IMongoCollection<BsonDocument> mongoCollection)
+		{
+			_mongoCollection = mongoCollection;
+			_registeredUserRepository = new UserRepository(connectionString);
+		}
+
+		public async Task<List<Review>> GetAllByIdOneToMany(int id)
+		{
+			var pipeline = new BsonDocument
+			{
+				{"$unwind", "$reviews"}
+			};
+
+			var pipeline2 = new BsonDocument
+			{
+				{"$match", new BsonDocument{
+					{"_id", id }
+				}}
+			};
+
+			var pipeline3 = new BsonDocument
+			{
+				{
+					"$project", new BsonDocument
+					{
+						{"_id", "$reviews._id" }
+						{"username", "$reviews.username"},
+						{"registeredUser_id", "$reviews.registeredUser_id"},
+						{"discription", "$reviews.discription"}
+					}
+				}
+			};
+
+			var pipelines = new BsonDocument[] { pipeline, pipeline2, pipeline3 };
+			List<BsonDocument> results = await _mongoCollection.Aggregate<BsonDocument>(pipelines).ToListAsync();
+
+			var reviews = new List<Review>();
+
+			foreach (BsonDocument item in results)
+			{
+				reviews.Add(new Review()
+				{
+					Id = item.GetValue("_id").ToInt32(),
+					RegisteredUser = _registeredUserRepository.GetById(item.GetValue("registeredUser_id").ToInt32()).Result,
+					Description = item.GetValue("discription").ToString()
+				});
+			}
+
+			return reviews;
+		}
+	}
+}
