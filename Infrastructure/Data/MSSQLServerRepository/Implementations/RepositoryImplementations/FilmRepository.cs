37eb37a76436543c703d37d0ed5da9dd3ed891f3
ImnChert <nikitatishkov228@yandex.ru>
2022-11-21 11:44:40 +0300

    careate film

--- Infrastructure/Data/MSSQLServerRepository/Implementations/RepositoryImplementations/FilmRepository.cs
+++ Infrastructure/Data/MSSQLServerRepository/Implementations/RepositoryImplementations/FilmRepository.cs
@@ -12,12 +12,21 @@ namespace Infrastructure.Data.MSSQLServerRepository.Implementations.MajorReposit
 	public class FilmRepository : MSSQLRepository<Film>
 	{
 		private IRepository<Distributor> _distributorRepository;
+		private readonly IRepository<OneToMany<Review>> _reviewRepository;
+		private readonly IRepository<OneToMany<Person>> _personRepository;
+		private readonly IRepository<OneToMany<Score>> _scoreRepository;
 		private readonly IGetAllById<OneToMany<Review>> _reviewGetAllById;
 		private readonly IGetAllById<OneToMany<Person>> _personGetAllById;
 		private readonly IGetAllById<OneToMany<Score>> _scoreGetAllById;
 
-		public FilmRepository(string connectionString, IRepository<Distributor> distributorRepository,
-			IGetAllById<OneToMany<Review>> reviewGetAllById, IGetAllById<OneToMany<Person>> personGetAllById, IGetAllById<OneToMany<Score>> scoreGetAllById)
+		public FilmRepository(string connectionString,
+			IRepository<Distributor> distributorRepository,
+			IRepository<OneToMany<Review>> reviewRepository,
+			IRepository<OneToMany<Person>> personRepository,
+			IRepository<OneToMany<Score>> scoreRepository,
+			IGetAllById<OneToMany<Review>> reviewGetAllById,
+			IGetAllById<OneToMany<Person>> personGetAllById,
+			IGetAllById<OneToMany<Score>> scoreGetAllById)
 			: base(connectionString,
 				  "Films",
 				  $@"INSERT INTO Films (Id,Name,Duration,DescriptionFilm,YearOfRelease,LicensExpirationDate,DistributorId,BasePrice) 
@@ -33,6 +42,9 @@ namespace Infrastructure.Data.MSSQLServerRepository.Implementations.MajorReposit
 					WHERE Id=@id")
 		{
 			_distributorRepository = distributorRepository;
+			_reviewRepository = reviewRepository;
+			_scoreRepository = scoreRepository;
+			_personRepository = personRepository;
 			_reviewGetAllById = reviewGetAllById;
 			_personGetAllById = personGetAllById;
 			_scoreGetAllById = scoreGetAllById;
@@ -41,9 +53,12 @@ namespace Infrastructure.Data.MSSQLServerRepository.Implementations.MajorReposit
 		public FilmRepository(string connectionString)
 			: this(connectionString,
 				 new DistributorRepository(connectionString),
-				 new ReviewShortRepository(connectionString),
-				 new PersonShortRepository(connectionString),
-				 new ScoreShortRepository(connectionString))
+				 new ReviewOneToManyRepository(connectionString),
+				 new PersonOneToManyRepository(connectionString),
+				 new ScoreOneToManyRepository(connectionString),
+				 new ReviewOneToManyRepository(connectionString),
+				 new PersonOneToManyRepository(connectionString),
+				 new ScoreOneToManyRepository(connectionString))
 		{
 		}
 
@@ -63,9 +78,8 @@ namespace Infrastructure.Data.MSSQLServerRepository.Implementations.MajorReposit
 				BasePrice = (decimal)sqlDataReader["BasePrice"]
 			};
 
-		protected override void InsertCommand(SqlCommand sqlCommand, Film entity)
+		protected override sealed void InsertCommand(SqlCommand sqlCommand, Film entity)
 		{
-			sqlCommand.Parameters.Add("@Id", SqlDbType.NVarChar).Value = entity.Id;
 			sqlCommand.Parameters.Add("@Name", SqlDbType.NVarChar).Value = entity.Name;
 			sqlCommand.Parameters.Add("@Duration", SqlDbType.NVarChar).Value = entity.Duration;
 			sqlCommand.Parameters.Add("@DescriptionFilm", SqlDbType.NVarChar).Value = entity.Description;
@@ -73,6 +87,33 @@ namespace Infrastructure.Data.MSSQLServerRepository.Implementations.MajorReposit
 			sqlCommand.Parameters.Add("@LicensExpirationDate", SqlDbType.NVarChar).Value = entity.LicensExpirationDate;
 			sqlCommand.Parameters.Add("@DistributorId", SqlDbType.NVarChar).Value = entity.Distributor.Id;
 			sqlCommand.Parameters.Add("@BasePrice", SqlDbType.NVarChar).Value = entity.BasePrice;
+		}
+
+		protected override async Task<bool> InsertSqlCommand(SqlCommand sqlCommand, Film entity)
+		{
+			InsertCommand(sqlCommand, entity);
+
+			entity.Scores.ForEach(score => _scoreRepository.InsertAsync(new OneToMany<Score>() { AdditionId = entity.Id, Value = score }));
+			entity.Reviews.ForEach(review => _reviewRepository.InsertAsync(new OneToMany<Review>() { AdditionId = entity.Id, Value = review }));
+			entity.FilmCrew.ForEach(person => _personRepository.InsertAsync(new OneToMany<Person>() { AdditionId = entity.Id, Value = person }));
+
+			await sqlCommand.ExecuteNonQueryAsync();
+
+			return true;
+		}
+
+		protected override async Task<bool> UpdateSqlCommand(SqlCommand sqlCommand, Film entity)
+		{
+			sqlCommand.Parameters.Add("@Id", SqlDbType.Int).Value = entity.Id;
+			InsertCommand(sqlCommand, entity);
+
+			entity.Scores.ForEach(score => _scoreRepository.UpdateAsync(new OneToMany<Score>() { AdditionId = entity.Id, Value = score }));
+			entity.Reviews.ForEach(review => _reviewRepository.UpdateAsync(new OneToMany<Review>() { AdditionId = entity.Id, Value = review }));
+			entity.FilmCrew.ForEach(person => _personRepository.UpdateAsync(new OneToMany<Person>() { AdditionId = entity.Id, Value = person }));
+
+			await sqlCommand.ExecuteNonQueryAsync();
+
+			return true;
 		}
 	}
 }
